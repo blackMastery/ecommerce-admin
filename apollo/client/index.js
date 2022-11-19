e17e65802db70e6a88e8f26e9c64a68906fc66b9
Kevon Cadogan <kev.cadogan300@gmail.com>
2022-11-19 12:44:06 -0400

    update

--- apollo/client/index.js
+++ apollo/client/index.js
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { ApolloClient } from '@apollo/client';
-    const { HttpLink } = require('@apollo/client/link/http');
+const { HttpLink } = require('@apollo/client/link/http');
 
 import { cache } from './cache';
 
@@ -19,21 +19,23 @@ let apolloClient;
 //   }
 // }
 
-
-
 const createHttpLink = (headers) => {
   const httpLink = new HttpLink({
-    uri: 'https://hasura-mallnet-graphql-api.herokuapp.com/v1/graphql',
+    uri: 'https://hasura.obsorb.net/v1/graphql',
     credentials: 'omit',
     headers: {
-      "Content-type": "application/json",
-      Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4ifSwidXNlcl9pZCI6IjEifQ.52vmkQ2QwrNIm-L8zCrun7MonC3mXY3_LFs6K4t9Ud4"
-    }
-     // auth token is fetched on the server side
+      'Content-type': 'application/json',
+      Authorization:
+        'Bearer eyJraWQiOiJCZDJZTXNvOFRrU1BoTXpHK2VNOWQ3ZHpSRXpuNEJYZzJhZmpTaVdEUmFjPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1ZDI2NTcyOC02MDAzLTQwODQtOTNhYi1mNjYwOGM1NTFmYmEiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaHR0cHM6XC9cL2hhc3VyYS5pb1wvand0XC9jbGFpbXMiOiJ7XCJ4LWhhc3VyYS11c2VyLWlkXCI6XCI1ZDI2NTcyOC02MDAzLTQwODQtOTNhYi1mNjYwOGM1NTFmYmFcIixcIngtaGFzdXJhLWRlZmF1bHQtcm9sZVwiOlwiYWRtaW5cIixcIngtaGFzdXJhLWFsbG93ZWQtcm9sZXNcIjpbXCJ1c2VyXCIsXCJhZG1pblwiXX0iLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9mTVdFZ1ZEZzgiLCJjb2duaXRvOnVzZXJuYW1lIjoiNWQyNjU3MjgtNjAwMy00MDg0LTkzYWItZjY2MDhjNTUxZmJhIiwib3JpZ2luX2p0aSI6ImFhMDUxOTBiLTI0OTEtNDhlNy04MGNkLTFiNTE2MDE5ODZiZiIsImF1ZCI6IjJzdGNybGdjOXYwZmMybDY3MnIyMDJpMzRkIiwiZXZlbnRfaWQiOiIxOGIxYmUxMS03MWFkLTQ4MGMtYmU1MS1hZmQ1ODBlNGNlNzgiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY1OTU4NDk0OSwiZXhwIjoxNjU5NTg4NTQ5LCJpYXQiOjE2NTk1ODQ5NDksImp0aSI6IjQ4YmY2MDhkLTY5ZjctNGI0NS04ZDBiLWU3MGI2YTljM2RjNCIsImVtYWlsIjoia2V2LmNhZG9nYW4zMDBAZ21haWwuY29tIn0.gGd2f9j9bIVLrspewNqKnD99uzp9EN77rO2YAXzQDkdEUEkVXYf-dWV-Fsv8LVhZOJrnuVUgrHADtkL2kmI4tN-P2p0dBzxuzoggDaqB194vuBR76bkAs2YFcQHn880-p-dAmGK8u6c1lYDOw1OlqHo8oZjAfvvjyUHEuECLoe5tW5TZvtGyWXm1p-fklEssD3lQxg0Bdc5F3S9InPPC-ujivR1UkgN-FRcpC_Fg07woONuK9UM9PU0LeK9Cb-i8jmljL6FqrTjpb09eHewvYw1vGmr8N-UyzhV7i7490riBokn8gHc402ArQyShoA7KvZAm_I6FiFGwHhRqVL9jmw',
+    },
+        fetchOptions: {
+      mode: 'no-cors',
+    },
+    // auth token is fetched on the server side
     // fetch,
-  })
+  });
   return httpLink;
-}
+};
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
 import { Button, Icon, Modaorm, Checkbox } from 'semantic-ui-react'
-import ProductTable from '../components/productTable/Product-Table'
 import { Grid, Menu, Segment } from 'semantic-ui-react'
-import { useQuery } from '@apollo/client';
-import { PRODUCTS, SORT_PRODUCT_SECTION } from '../apollo/client/queries';
+import { useQuery, gql } from '@apollo/client';
 
 
 function Index  () { 
 
+  const GET_MY_TODOS = gql`
+  query MyQuery {
+    person {
+      email
+    }
+  }
+`;
 
+const { loading, error, data } = useQuery(GET_MY_TODOS);
+
+console.log({  loading, error, data });
   
  return (
  <Segment>
-              <ProductTable/>
+             
   </Segment>
 )}
 
